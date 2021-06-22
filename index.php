1108055100674e21a350182b199c265e2b18034d
Sa Yaneli <Yaan.19.22@gmail.com>
2021-06-22 16:06:44 -0500

    SNAPSHOT 002 del proyecto, sólo modificamos index

--- index.php
+++ index.php
@@ -1,1 +1,15 @@
-index.php
+<html>
+<head>
+
+<body>
+   <marquee bgcolor="pink">¡Saludos Cordiales TICsitos!</marquee>
+
+   <br>
+   En el proyecto que vamos a realizar, es desarrollar una aplicación web con el fin de apoyar el proceso de lecciones de estudiantes, así como proyectos universitarios de la Universidad Tecnológica de la Selva, Rayón Chiapas. Tratará de obtener un producto que pueda ser aplicable y modernice el sistema de votaciones actual que está basado en la estructura tradicional en hojas de papel y marcación de boletas, nos e busca eliminar el uso de los stands o cubículos para votar si no mejorar el proceso a través de la incorporación de los recursos tecnológicos disponibles. Estamos seguros que será de gran ayuda y beneficio para la universidad de contar con un sistema así.
+   <br>
+  </body>
+<br>
+
+
+  </head>
+</html>
